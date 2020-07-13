5482adc27c504211f7b25fa3d641be7dc07f1e11
imanzto01 <46841419+imanzto01@users.noreply.github.com>
2020-07-14 03:38:52 +0700

    Create Main Activity.java

---
+++ Main Activity.java
@@ -0,0 +1,30 @@
+package com.example.daftarrumahsakit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnMoveActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMoveActivity = (Button) findViewById(R.id.btn_daftar);
+        btnMoveActivity.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_daftar:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+        }
+    }
+}
